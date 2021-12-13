ace5a57f3cf772bf07ffa11fd770d86f50628672
Sarah Nani <sarahlenani@gmail.com>
2021-12-13 11:12:24 -0300

    Modificando exercício06 da lista03 (lista03 finalizada)

--- eclipse/lista03/Exercicio06.java
+++ eclipse/lista03/Exercicio06.java
@@ -8,17 +8,26 @@ public class Exercicio06 {
 		
 		Scanner in = new Scanner(System.in);
 		
-		int numero, soma = 0;
+		int numero = 1, soma = 0, media = 0, quantidade = 0;
 		
-		for (int i = 1; i != 0; i++) {
-			System.out.printf("Insira o %dº número: ", i);
-			numero = in.nextInt();
-			
-			if (numero % 3 == 0) {
-				soma += numero;
+		do {
+			for (int i = 1; i < 10; i++) {
+				System.out.printf("Insira o %dº número: ", i);
+				numero = in.nextInt();
+				
+				if (numero == 0) {
+					System.out.println("Sessão encerrada!");
+					break;
+				} else if (numero % 3 == 0) {
+					soma += numero;
+					quantidade++;
+				}
 			}
-		}
-		
+		} while (numero != 0);
+				
+		media = soma / quantidade;
+		System.out.printf("\nA média dos números múltiplos de 3 é: %d", media);
+		in.close();
 
 	}
 
