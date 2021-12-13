8cd2905a5014f64bb43f287445c72ba8761e7dd2
Sarah Nani <sarahlenani@gmail.com>
2021-12-13 10:55:30 -0300

    Modificando exercício05 da lista01 (lista01 finalizada)

--- eclipse/lista01/Exercicio05.java
+++ eclipse/lista01/Exercicio05.java
@@ -1,10 +1,29 @@
 package br.com.generation.lista01;
 
-public class Exercicios05 {
+import java.util.Scanner;
+
+public class Exercicio05 {
 
 	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
 		
+		double nota1, nota2, nota3, media;
 		
+		System.out.print("Digite a sua 1ª nota: ");
+		nota1 = in.nextDouble();
+		
+		System.out.print("Digite a sua 2ª nota: ");
+		nota2 = in.nextDouble();
+		
+		System.out.print("Digite a sua 3ª nota: ");
+		nota3 = in.nextDouble();
+		
+		media = (nota1 * 0.2) + (nota2 * 0.3) + (nota3 * 0.5);
+		
+		System.out.printf("\nSuas notas foram: %.2f, %.2f e %.2f com peso 2, 3 e 5 respectivamente."
+				+ "\nPortanto sua média é %.2f", nota1, nota2, nota3, media);
+		in.close();
 
 	}
 
